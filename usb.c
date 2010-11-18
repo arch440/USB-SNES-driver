bdbb9269a274c2fe71ab40932ec8fec00435c398
Mats Borjesson <risk2210ad@gmail.com>
2010-11-18 15:18:50 -0800

    Modular

--- usb.c
+++ usb.c
@@ -3,38 +3,38 @@
 
 void printdev();
    
-int main(){   
-    
-    
-    
+int main(){
     usb_init();
     usb_find_busses();
     usb_find_devices();
     struct usb_bus* bus;
     bus = usb_get_busses();
+    struct usb_device* dev;
+	printdev(bus, dev);
+}
+
+void printdev(struct usb_bus* bus, struct usb_device* dev){
     int ret;
     char string[512];
-    struct usb_device* dev;
-
-
     printf("bus/device idVendor/idProduct\n");
-    for (; bus != NULL; bus = bus->next) {
-        for (dev = bus->devices; dev; dev = dev->next) {
+    for(; bus != NULL; bus = bus->next){
+        for(dev = bus->devices; dev; dev = dev->next){
             usb_dev_handle *udev;
             printf("%s/%s %04X/%04X\n", bus->dirname, dev->filename,
             dev->descriptor.idVendor, dev->descriptor.idProduct);
            	udev = usb_open(dev);
-           	if (udev) {
-        		if (dev->descriptor.iManufacturer) {
+           	if(udev){
+        		if(dev->descriptor.iManufacturer){
          			ret = usb_get_string_simple(udev, dev->descriptor.iManufacturer,
             		string, sizeof (string));
-            		if (ret > 0)
-           				printf("- Manufacturer : %s\n", string);
+            		if(ret > 0)
+           				printf("Manufacturer: %s\n", string);
             		else
-                		printf("- Unable to fetch manufacturer string\n");
+                		printf("Unable to fetch manufacturer string\n");
                 }
         	}
     	}
 	}
 }
+
         
