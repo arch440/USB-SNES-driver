d8988565d98b12efe6d6f35f184716fba7ec66d6
Mats Borjesson <risk2210ad@gmail.com>
2010-11-25 20:28:06 +0100

    Cleanup

--- usb.c
+++ usb.c
@@ -1,40 +1,45 @@
-#include <usb.h>
-#include <stdio.h>
+#include "usb.h"
 
-void printdev();
-   
-int main(){
+int main(void){
     usb_init();
     usb_find_busses();
     usb_find_devices();
-    struct usb_bus* bus;
-    bus = usb_get_busses();
-    struct usb_device* dev;
 	printdev(bus, dev);
+	probedev(bus, dev);
+}
+
+void probedev(struct usb_bus* bus, struct usb_device* dev){
+	int i = 0;
+	for(bus = usb_get_busses(); bus != NULL; bus = bus->next){
+    	for(dev = bus->devices; dev != NULL; dev = dev->next){
+			printf("VID%d: %x\nPID%d: %x\n", i, dev->descriptor.idVendor,
+			i, dev->descriptor.idProduct);
+			i++;
+		}
+	}	
 }
 
 void printdev(struct usb_bus* bus, struct usb_device* dev){
-    int ret;
-    char string[512];
     printf("bus/device idVendor/idProduct\n");
-    for(; bus != NULL; bus = bus->next){
-        for(dev = bus->devices; dev; dev = dev->next){
-            usb_dev_handle *udev;
+    for(bus = usb_get_busses(); bus != NULL; bus = bus->next){
+        for(dev = bus->devices; dev != NULL; dev = dev->next){
             printf("%s/%s %04X/%04X\n", bus->dirname, dev->filename,
             dev->descriptor.idVendor, dev->descriptor.idProduct);
-           	udev = usb_open(dev);
+			udev = usb_open(dev);
            	if(udev){
         		if(dev->descriptor.iManufacturer){
-         			ret = usb_get_string_simple(udev, dev->descriptor.iManufacturer,
-            		string, sizeof (string));
+         			int ret = usb_get_string_simple(
+         			udev,dev->descriptor.iManufacturer, string, sizeof (string));
             		if(ret > 0)
            				printf("Manufacturer: %s\n", string);
             		else
                 		printf("Unable to fetch manufacturer string\n");
-                }
+                } else
+                	printf("No manufacturer descriptor\n");
         	}
+        	usb_close(udev);
     	}
-	}
+    }
 }
 
         
--- usb.h
+++ usb.h
@@ -1,8 +1,12 @@
-tatic struct usb_driver skel_driver = {
-    name:        "skeleton",
-    probe:       skel_probe,
-    disconnect:  skel_disconnect,
-    fops:        &skel_fops,
-    minor:       USB_SKEL_MINOR_BASE,
-    id_table:    skel_table,
-};
+#include <usb.h>
+#include <stdio.h>
+#define MID 0x0000
+#define VID 0x0000
+
+char string[256];
+struct usb_bus* bus;
+struct usb_device* dev;
+usb_dev_handle* udev;
+
+void printdev();
+void probedev();
